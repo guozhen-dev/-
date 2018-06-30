bc73e03dd19cb7f71a042c0212538f1bb4d9f448
丁国真 <gzh.ding@outlook.com>
2018-06-30 12:09:07 +0800

    commit
    first time

--- 20180629/FirstViewController.swift
+++ 20180629/FirstViewController.swift
@@ -7,19 +7,144 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+import CoreBluetooth
+//import BabyBlueTooth
 
 class FirstViewController: UIViewController {
+    
+//    var baby: BabyBluetooth?
+    
+    /*
 
+    func babyDelegate() {
+        //设置扫描到设备的委托
+        baby.blockOnDiscoverToPeripherals = { central, peripheral, advertisementData, RSSI in
+            print("搜索到了设备:\(peripheral?.name ?? "")")
+        }
+        //过滤器
+        //设置查找设备的过滤器
+        baby!.filterOnDiscoverPeripherals = { peripheralName, advertisementData, RSSI in
+            //最常用的场景是查找某一个前缀开头的设备
+            //if ([peripheralName hasPrefix:@"Pxxxx"] ) {
+            //    return YES;
+            //}
+            //return NO;
+            //设置查找规则是名称大于1 ， the search rule is peripheral.name length > 1
+            if (peripheralName?.count ?? 0) > 1 {
+                return true
+            }
+            return false
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        super.viewDidLoad()
+//        //初始化BabyBluetooth 蓝牙库
+//        baby = BabyBluetooth.share()
+//        //设置蓝牙委托
+//        babyDelegate()
+//        //设置委托后直接可以使用，无需等待CBCentralManagerStatePoweredOn状态
+//        baby?.scanForPeripherals().begin()
     }
-
+ */
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//
+//    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var input_value: UITextField!
+    @IBAction func Submitt(_ sender: UIButton) {
+        print("Button Pressed")
+        
+        let input = input_value.text!
+       
+//            let videoURL = "http://172.18.16.92/1.mp4"
+            
+            
+            
+            let manager = FileManager.default
+            let urlForDocument = manager.urls(for: .documentDirectory, in:.userDomainMask)
+            let docPath = urlForDocument[0]
+            let file = docPath.appendingPathComponent("chk.txt")
+            
+            let readHandler = try! FileHandle(forReadingFrom:file)
+            let data = readHandler.readDataToEndOfFile()
+            let readString = String(data: data, encoding: String.Encoding.utf8)
+            print("文件内容: \(readString)")
+            var chk = Int(readString!)!
+            if chk == 1{
+                let player = AVPlayer(url:URL(string: "http://172.20.10.2/1.mp4")!)
+                let vc = AVPlayerViewController()
+                vc.player = player
+                present(vc, animated: true) {
+                    vc.player?.play()
+                }
+            }else  {
+                    let alertController = UIAlertController(title: "您未到达该地",
+                                                            message: nil, preferredStyle: .alert)
+                    //显示提示框
+                    self.present(alertController, animated: true, completion: nil)
+                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+                        self.presentedViewController?.dismiss(animated: false, completion: nil)
+                        
+                    }
+                    //2秒钟之后自动消失对话框
+                }
+            
+            
+            
+            
+        
+        
+    }
+   
+    
 
+    
+    @IBAction func choose(_ sender: Any) {
+//        super.viewDidAppear(animated)
+        
+        
+        
+        
 
+        
+        let alertController = UIAlertController(title: "系统提示",message: "您确定要设置法国为目的地吗？", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        let okAction = UIAlertAction(title: "好的", style: .default, handler: {
+            action in
+            print("点击了确定")
+        })
+        
+        let chkfilePath:String = NSHomeDirectory() + "/Documents/chk.txt"
+        let chkinfo = String("0")
+        //            let srcUrl = NSHomeDirectory() + "/Documents/hangge.txt"
+        //            try! fileManager.removeItem(atPath: srcUrl)
+        try! chkinfo.write(toFile: chkfilePath, atomically: true, encoding: String.Encoding.utf8)
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+        let manager = FileManager.default
+        let urlForDocument = manager.urls(for: .documentDirectory, in:.userDomainMask)
+        let url = urlForDocument[0] as URL
+        print(url)
+        let filePath:String = NSHomeDirectory() + "/Documents/hangge.txt"
+        let info = "0"
+        try! info.write(toFile: filePath, atomically: true, encoding: String.Encoding.utf8)
+        
+        
+    }
+    
 }
+
 
--- 20180629/SecondViewController.swift
+++ 20180629/SecondViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import CoreBluetooth
+
+
+private var peripheralManager: CBPeripheralManager?
+private var characteristic: CBMutableCharacteristic?
+public var tot_dis = 0
+
+
+
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet weak var loadImageFromURL: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +30,104 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    public func reset_dis(){
+        tot_dis = tot_dis - tot_dis
+    }
+    
+    @IBAction func Add_dis(_ sender: Any) {
+        let manager = FileManager.default
+        let urlForDocument = manager.urls(for: .documentDirectory, in:.userDomainMask)
+        let docPath = urlForDocument[0]
+        let file = docPath.appendingPathComponent("hangge.txt")
 
+        let readHandler = try! FileHandle(forReadingFrom:file)
+        let data = readHandler.readDataToEndOfFile()
+        let readString = String(data: data, encoding: String.Encoding.utf8)
+        print("文件内容: \(readString)")
+        tot_dis = Int(readString!)!
+        let alertController = UIAlertController(title: "今日步数",
+                                                message: "请输入今天走的步数", preferredStyle: .alert)
+        alertController.addTextField {
+            (textField: UITextField!) -> Void in
+            textField.placeholder = "今天步数"
+        }
+        let okAction = UIAlertAction(title: "好的", style: .default, handler: {
+            action in
+            let login = alertController.textFields!.first!
+            let get_int = Int(login.text!)
+            print("您输入的步数：\(get_int)")
+            tot_dis = tot_dis + get_int!
+            var url1 :URL
+            if tot_dis <= 5000 {
+                url1 = URL(string: "https://s1.ax1x.com/2018/06/30/PFDt2Q.png")!
+            } else if  tot_dis <= 10000{
+                url1 = URL(string: "https://s1.ax1x.com/2018/06/30/PFDY8g.png")!
+            } else if tot_dis <= 15000{
+                url1 = URL(string: "https://s1.ax1x.com/2018/06/30/PFDaKs.png")!
+            } else {
+                 url1 = URL(string: "https://s1.ax1x.com/2018/06/30/PFDc24.png")!
+            }
+            
+            let data = try? Data(contentsOf: url1)
+            
+            if let imageData = data {
+                let image = UIImage(data: data!)
+                self.loadImageFromURL.image = image
+            }
+            print ("tot :\(tot_dis)")
+            if tot_dis >= 20000 {
+                let chkfilePath:String = NSHomeDirectory() + "/Documents/chk.txt"
+                let chkinfo = String("1")
+                //            let srcUrl = NSHomeDirectory() + "/Documents/hangge.txt"
+                //            try! fileManager.removeItem(atPath: srcUrl)
+                try! chkinfo.write(toFile: chkfilePath, atomically: true, encoding: String.Encoding.utf8)
+                print ("Writein")
+                print (chkinfo)
+                let alertController = UIAlertController(title: "恭喜你，已经到达法国",
+                                                        message: nil, preferredStyle: .alert)
+                //显示提示框
+                self.present(alertController, animated: true, completion: nil)
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+                    self.presentedViewController?.dismiss(animated: false, completion: nil)
+                    
+                }
+                //2秒钟之后自动消失对话框
+            }
+            let filePath:String = NSHomeDirectory() + "/Documents/hangge.txt"
+            let info = String(tot_dis)
+//            let srcUrl = NSHomeDirectory() + "/Documents/hangge.txt"
+//            try! fileManager.removeItem(atPath: srcUrl)
+            try! info.write(toFile: filePath, atomically: true, encoding: String.Encoding.utf8)
+        })
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
 
+        
+    }
+   
+    
+    
+    
+    @IBAction func Index(_ sender: Any) {
+        let manager = FileManager.default
+        let urlForDocument = manager.urls(for: .documentDirectory, in:.userDomainMask)
+        let docPath = urlForDocument[0]
+        let file = docPath.appendingPathComponent("hangge.txt")
+        
+        let readHandler = try! FileHandle(forReadingFrom:file)
+        let data = readHandler.readDataToEndOfFile()
+        let readString = String(data: data, encoding: String.Encoding.utf8)
+        print("文件内容: \(readString)")
+        let alertController = UIAlertController(title: readString,
+                                                message: nil, preferredStyle: .alert)
+        //显示提示框
+        self.present(alertController, animated: true, completion: nil)
+        //两秒钟后自动消失
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+            self.presentedViewController?.dismiss(animated: false, completion: nil)
+        }
+    }
+    
+    
 }
 
